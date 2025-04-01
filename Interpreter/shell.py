ad760816204e128b3d773d1204310981dff45210
rdb231-com231 <flaviofdbastos@gmail.com>
2025-04-01 16:33:55 -0300

    CLI

--- Interpreter/shell.py
+++ Interpreter/shell.py
@@ -1,19 +1,24 @@
-import basic
+import interpreter
 import time
 
 reopen = False
 
-while True:
-		text = input('Nexus > ')
-		if text.strip() == "": continue	
-		if text == 'close':
-			print('\033c', end='')
-			break
-		if text == 'exit':
-			break
-		if text == 'clear' or text == 'cls':
-			print('\033c', end='')
-			continue
-		result, error = basic.run('<stdin>', text)
+def start():
+	while True:
+			text = input('Nexus > ')
+			if text.strip() == "": continue	
+			if text == 'close':
+				print('\033c', end='')
+				break
+			if text == 'exit':
+				break
+			if text == 'clear' or text == 'cls':
+				print('\033c', end='')
+				continue
+			if text == 'help':
+				print("Please provide a code following Nexus Syntax Guidelines or use: exit (exits the shell), clear/cls (clears the terminal), close (closes and clears the terminal).")
+			result, error = interpreter.run('<stdin>', text)
 
-		if error: print(error.as_string())
+			if error: print(error.as_string())
+
+start()
